41b27e2003755d0e22db62fa7861630826fd2bfc
ArnasMarozas <maraskaras123@gmail.com>
2016-12-25 18:13:43 +0200

    better design for logout.php

--- scripts/clickRow.js
+++ scripts/clickRow.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    var link = "http://belekas.esy.es/view.php?id=";
+    var link = "http://belekas.esy.es/visit.php?id=";
     $('.visit tbody tr').click(function(){ 
         var fullLink = link + $(this).attr('id');
         window.location = fullLink;
