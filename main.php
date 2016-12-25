6ada78607f61fd745af8ace1884597bded342544
ArnasMarozas <maraskaras123@gmail.com>
2016-12-25 18:17:30 +0200

    added "create client" and "create visit" buttons, changed style of "logout button". Added visit.php file for visit info

--- main.php
+++ main.php
@@ -15,33 +15,25 @@ if(!$fgmembersite->CheckLogin())
 		<script src="scripts/clickRow.js"></script>
 	</head>
 	<body>
-		<p class="main-logout" align="right"><a href='logout.php'><button>logout</button></a></p>
+		<p class="main-logout">
+			<a href='createClient.php' style='margin-right: 4.5%;'><button style="background: #3DBBFF;">create client</button></a>
+			<a href='createVisit.php'><button style="background: #3DBBFF;">create visit</button></a>
+			<a href='logout.php' style='margin-left: 4.5%;'><button>logout</button></a>
+		</p>
 		<div style="overflow-x:auto;">
 			<table class="visit">
 				<thead>
 					<tr>
-						<th>Vardas</th>
-						<th>Pavarde</th>
-						<th>Data</th>
-						<th>Produktas/kiekis</th>
-						<th>Kaina</th>
+						<th>Name</th>
+						<th>Surname</th>
+						<th>Date</th>
+						<th>Number</th>
 					</tr>
 				</thead>
 				<tbody>
-					<tr id="0">
-						<td>Rasa</td>
-						<td>Maroziene</td>
-						<td>2016-09-17</td>
-						<td>3/1.5</td>
-						<td>100€</td>
-					</tr>
-					<tr id="1">
-						<td>Arnas</td>
-						<td>Marozas</td>
-						<td>2016-10-1</td>
-						<td>2/0.5</td>
-						<td>50€</td>
-					</tr>
+					<tr id="0"><td>Rasa</td><td>Maroziene</td><td>2016-09-17</td><td>+37065599996</td></tr>
+					<tr id="1"><td>Arnas</td><td>Marozas</td><td>2016-10-01</td><td>+37064794814</td></tr>
+					<tr id="2"><td>Jurijus</td><td>Marozas</td><td>2016-12-21</td><td>+37064400063</td></tr>
 				</tbody>
 			</table>
 		</div>
