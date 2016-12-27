b5aadd2480372e62f8b4ec9d7b15cc55b252e540
ArnasMarozas <maraskaras123@gmail.com>
2016-12-27 04:37:03 +0200

    full test implement for visit.php, design changed from green to yellow

--- visit.php
+++ visit.php
@@ -13,10 +13,33 @@ if(!$fgmembersite->CheckLogin()) {
 		<title>visit info</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     </head>
-    <body>
-        Hello!
-        <a download="8_0.jpg" href="photos/8_0.jpg" title="ImageName">
-            <img alt="8_0" src="photos/8_0.jpg"/>
-        </a>
+    <body style="font-size: 30px; font-weight: bold; text-align: center;">
+        <div class="form" style="margin-bottom: 10px; padding: 2px;">
+            Rasa Maroziene
+            <br>
+            2016-12-28
+            <br>
+            type: cba
+            <br>
+            amount: 1.5
+            <br>
+            price: €45
+        </div>
+        Before photos:
+        <br>
+        <a download="8_0.jpg" href="photos/8_0.jpg" title="ImageName"><img alt="8_0" src="photos/8_0.jpg"/></a>
+        <br>
+        After photos:
+        <br>
+        <a download="8_0.jpg" href="photos/8_0.jpg" title="ImageName"><img alt="8_0" src="photos/8_0.jpg"/></a>
+        <a download="8_0.jpg" href="photos/8_0.jpg" title="ImageName"><img alt="8_0" src="photos/8_0.jpg"/></a>
+        <a download="8_0.jpg" href="photos/8_0.jpg" title="ImageName"><img alt="8_0" src="photos/8_0.jpg"/></a>
+        <a download="8_0.jpg" href="photos/8_0.jpg" title="ImageName"><img alt="8_0" src="photos/8_0.jpg"/></a>
+        <a download="8_0.jpg" href="photos/8_0.jpg" title="ImageName"><img alt="8_0" src="photos/8_0.jpg"/></a>
+        <a download="8_0.jpg" href="photos/8_0.jpg" title="ImageName"><img alt="8_0" src="photos/8_0.jpg"/></a>
+        <a download="8_0.jpg" href="photos/8_0.jpg" title="ImageName"><img alt="8_0" src="photos/8_0.jpg"/></a>
+        <a download="8_0.jpg" href="photos/8_0.jpg" title="ImageName"><img alt="8_0" src="photos/8_0.jpg"/></a>
+        <a download="8_0.jpg" href="photos/8_0.jpg" title="ImageName"><img alt="8_0" src="photos/8_0.jpg"/></a>
+        <a download="8_0.jpg" href="photos/8_0.jpg" title="ImageName"><img alt="8_0" src="photos/8_0.jpg"/></a>
     </body>
 </html>
