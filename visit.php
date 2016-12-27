f4fa5dd8d5201e14b7b49fb1a584ce08495e199d
ArnasMarozas <maraskaras123@gmail.com>
2016-12-27 03:24:08 +0200

    test photo and style for visit.php

--- visit.php
+++ visit.php
@@ -9,10 +9,14 @@ if(!$fgmembersite->CheckLogin()) {
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="style/style.css" />
+        <link rel="stylesheet" type="text/css" href="style/visit.css" />
 		<title>visit info</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     </head>
     <body>
         Hello!
+        <a download="8_0.jpg" href="photos/8_0.jpg" title="ImageName">
+            <img alt="8_0" src="photos/8_0.jpg"/>
+        </a>
     </body>
 </html>
